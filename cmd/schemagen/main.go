e150ad9bc5d4554058675b299568e975971193af
James Strachan <james.strachan@gmail.com>
2021-04-26 14:14:54 +0100

    fix: better schema gen

--- cmd/schemagen/main.go
+++ cmd/schemagen/main.go
@@ -1,81 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
+	"github.com/jenkins-x-plugins/jx-charter/pkg/apis/chart"
 	"github.com/jenkins-x-plugins/jx-charter/pkg/apis/chart/v1alpha1"
-	"github.com/jenkins-x/jx-api/v4/pkg/util"
+	"github.com/jenkins-x/jx-api/v4/pkg/schemagen"
 	"github.com/jenkins-x/jx-logging/v3/pkg/log"
-	"github.com/pkg/errors"
-	"io/ioutil"
 	"os"
 )
 
-func main() {
-	o := &Options{}
-	if len(os.Args) > 1 {
-		o.Out = os.Args[1]
+var (
+	resourceKinds = []schemagen.ResourceKind{
+		{
+			APIVersion: chart.GroupAndVersion,
+			Name:       "chart",
+			Resource:   &v1alpha1.Chart{},
+		},
 	}
-	err := o.Run()
+)
+
+func main() {
+	out := "schema"
+	if len(os.Args) > 1 {
+		out = os.Args[1]
+	}
+	err := schemagen.GenerateSchemas(resourceKinds, out)
 	if err != nil {
 		log.Logger().Errorf("failed: %v", err)
 		os.Exit(1)
 	}
 	log.Logger().Infof("completed the plugin generator")
 	os.Exit(0)
-}
-
-type Options struct {
-	Out string
-}
-
-// Run implements this command
-func (o *Options) Run() error {
-	if o.Out == "" {
-		o.Out = "schema/chart.json"
-	}
-	return o.Generate("chart.yml", &v1alpha1.Chart{})
-}
-
-// Generate generates the schema document
-func (o *Options) Generate(schemaName string, schemaTarget interface{}) error {
-	schema := util.GenerateSchema(schemaTarget)
-	if schema == nil {
-		return fmt.Errorf("could not generate schema for %s", schemaName)
-	}
-
-	output := prettyPrintJSON(schema)
-
-	if output == "" {
-		tempOutput, err := json.Marshal(schema)
-		if err != nil {
-			return errors.Wrapf(err, "error outputting schema for %s", schemaName)
-		}
-		output = string(tempOutput)
-	}
-	log.Logger().Infof("JSON schema for %s:", schemaName)
-
-	if o.Out != "" {
-		err := ioutil.WriteFile(o.Out, []byte(output), util.DefaultWritePermissions)
-		if err != nil {
-			return errors.Wrapf(err, "failed to save file %s", o.Out)
-		}
-		log.Logger().Infof("wrote file %s", o.Out)
-		return nil
-	}
-	log.Logger().Infof("%s", output)
-	return nil
-}
-
-func prettyPrintJSON(input interface{}) string {
-	output := &bytes.Buffer{}
-	if err := json.NewEncoder(output).Encode(input); err != nil {
-		return ""
-	}
-	formatted := &bytes.Buffer{}
-	if err := json.Indent(formatted, output.Bytes(), "", "  "); err != nil {
-		return ""
-	}
-	return string(formatted.Bytes())
 }
